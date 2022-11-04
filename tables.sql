74d42bf9e123a3986c61f87cd4fc273628a63a12
JazMac <jazmine.n.hudson@gmail.com>
2022-11-04 15:24:25 -0500

    initialized repo

---
+++ tables.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS users;
+--  TABLE CALLED USERS VVVV
+CREATE TABLE users (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(35) NOT NULL, -- colum named first_name with its own SQL data types VARCHAR(35) meaning text up to 35 charters
+    last_name VARCHAR(65) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+--
+-- CREATE TABLE users (
+--     user_id INT NOT NULL AUTO_INCREMENT,
+--     first_name VARCHAR(35) NOT NULL,
+--     last_name VARCHAR(65) NOT NULL,
+--     PRIMARY KEY (user_id)
+-- );
