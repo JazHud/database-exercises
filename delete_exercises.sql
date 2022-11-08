10b72abc05b8d0f8671dc4459453079b2dda0c4b
JazMac <jazmine.n.hudson@gmail.com>
2022-11-08 09:07:09 -0600

    add comment

---
+++ delete_exercises.sql
@@ -0,0 +1,21 @@
+# delete exercises
+USE codeup_test_db;
+
+# Write SELECT statements for:
+    # Albums released after 1991
+SELECT * From albums;
+    SELECT album_name FROM albums WHERE release_date > 1991;
+    DELETE FROM albums WHERE release_date > 1991;
+
+
+    # Albums with the genre 'disco'
+    SELECT album_name FROM albums WHERE genre = 'disco';
+    DELETE FROM albums WHERE genre = 'disco';
+
+
+    # Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+    SELECT album_name FROM albums WHERE artist_name = 'Whitney Houston';
+    DELETE FROM albums WHERE artist_name = 'Whitney Houston';
+# Make sure to put appropriate captions before each SELECT.
+# Convert the SELECT statements to DELETE.
+# Use the MySQL command line client to make sure your records really were removed.
